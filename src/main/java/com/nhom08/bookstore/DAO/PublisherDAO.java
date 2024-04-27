42e91631face32fc87c999b9dc4acb7647a78bc0
qdatn <datnq2003@gmail.com>
2024-04-27 13:58:30 +0700

    Publisher DAO

--- src/main/java/com/nhom08/bookstore/DAO/PublisherDAO.java
+++ src/main/java/com/nhom08/bookstore/DAO/PublisherDAO.java
@@ -4,10 +4,128 @@
  */
 package com.nhom08.bookstore.DAO;
 
+import com.nhom08.bookstore.Models.PublisherModel;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author quythuong
  */
 public class PublisherDAO {
-	
+
+    private Connection con = null;
+
+    public PublisherDAO() {
+        con = (new DBConnection()).GetDBConnection();
+    }
+
+    public List<PublisherModel> getAll() throws SQLException {
+        List<PublisherModel> publisherList = new ArrayList<PublisherModel>();
+        PreparedStatement statement = null;
+        ResultSet rs = null;
+
+        try {
+            statement = con.prepareStatement("select * from NhaXuatBan");
+            rs = statement.executeQuery();
+            while (rs.next()) {
+                PublisherModel publisher = new PublisherModel();
+
+                publisher.setId(rs.getString(1));
+                publisher.setName(rs.getString(2));
+                publisher.setAddress(rs.getString(3));
+                publisher.setContact(rs.getString(4));
+
+                publisherList.add(publisher);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return publisherList;
+    }
+
+    public PublisherModel getOne(String id) throws SQLException {
+        PublisherModel publisher = new PublisherModel();
+        PreparedStatement statement = null;
+        ResultSet rs = null;
+
+        try {
+            statement = con.prepareStatement("select * from NhaXuatBan where MaNXB = ?");
+            statement.setString(1, id);
+            rs = statement.executeQuery();
+            while (rs.next()) {
+                publisher.setId(rs.getString(1));
+                publisher.setName(rs.getString(2));
+                publisher.setAddress(rs.getString(3));
+                publisher.setContact(rs.getString(4));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return publisher;
+    }
+
+    public boolean delete(String id) throws SQLException {
+        CallableStatement statement = null;
+        ResultSet rs = null;
+        boolean hasResultSet = false;
+
+        try {
+            statement = con.prepareCall("{ call Proc_XoaNhaXuatBan(?) }");
+            statement.setString(1, id);
+
+            hasResultSet = statement.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return hasResultSet;
+    }
+
+    public boolean add(PublisherModel publisher) throws SQLException {
+        CallableStatement statement = null;
+        ResultSet rs = null;
+        boolean hasResultSet = false;
+
+        try {
+            statement = con.prepareCall("{ call Proc_ThemNhaXuatBan(?,?,?,?) }");
+            statement.setString(1, publisher.getId());
+            statement.setString(2, publisher.getName());
+            statement.setString(3, publisher.getAddress());
+            statement.setString(4, publisher.getContact());
+
+            hasResultSet = statement.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return hasResultSet;
+    }
+
+    public boolean update(PublisherModel publisher) throws SQLException {
+        CallableStatement statement = null;
+        ResultSet rs = null;
+        boolean hasResultSet = false;
+
+        try {
+            statement = con.prepareCall("{ call Proc_SuaNhaXuatBan(?,?,?,?) }");
+            statement.setString(1, publisher.getId());
+            statement.setString(2, publisher.getName());
+            statement.setString(3, publisher.getAddress());
+            statement.setString(4, publisher.getContact());
+
+            hasResultSet = statement.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return hasResultSet;
+    }
+
 }
